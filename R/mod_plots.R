90e06075e7d44de5c666d0a80587800d96b58f31
Chris Beeley <chris.beeley@gmail.com>
2021-03-23 15:24:57 +0000

    Ugly bar graph

--- R/mod_plots.R
+++ R/mod_plots.R
@@ -13,13 +13,23 @@ mod_plots_ui <- function(id){
     
     fluidRow(
       column(3,
-             ),
+      ),
       column(9,
-             plotOutput(ns("first")))
+             tabsetPanel(
+               tabPanel("Scatter plot",
+                        plotOutput(ns("scatter"))
+               ),
+               tabPanel("Bar graph",
+                        sliderInput(ns("penguin_number"), "Penguin numbers",
+                                    min = 5, max = 200, value = 25),
+                        plotOutput(ns("bargraph"))
+               )
+             )
+      )
     )
   )
 }
-    
+
 #' plots Server Functions
 #'
 #' @noRd 
@@ -27,19 +37,32 @@ mod_plots_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
     
-    output$first <- renderPlot({
+    output$scatter <- renderPlot({
       
       palmerpenguins::penguins %>% 
         ggplot2::ggplot(ggplot2::aes(x = bill_length_mm,
                                      y = flipper_length_mm)) + 
         ggplot2::geom_point()
     })
- 
+    
+    output$bargraph <- renderPlot({
+      
+      penguin_names <- paste(c("Steve", "Mark", "Karen"), 1 : 400)
+      
+      palmerpenguins::penguins %>% 
+        dplyr::mutate(name = sample(penguin_names, 
+                                    nrow(palmerpenguins::penguins), 
+                                    replace = FALSE)) %>% 
+        dplyr::sample_n(input$penguin_number) %>% 
+        ggplot2::ggplot(ggplot2::aes(x = name, y = flipper_length_mm)) + 
+        ggplot2::geom_bar(stat = "identity")
+    })
+    
   })
 }
-    
+
 ## To be copied in the UI
 # 
-    
+
 ## To be copied in the server
 # 
